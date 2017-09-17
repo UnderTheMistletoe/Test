e7fdc4bf715d724aa0005a485528edc15dc2d1eb
withoutMistake <zt1996816@qq.com>
2017-09-17 22:51:11 +0800

    可能数运算还产生double类型数据

--- src/Test.java
+++ src/Test.java
@@ -1,18 +1,27 @@
 import java.util.LinkedList;
 
+
 public class Test {
 
     private LinkedList<Integer> numStack = new LinkedList<>();
-    private LinkedList<Character> opStack = new LinkedList<>();
+    private LinkedList<String> opStack = new LinkedList<>();
 
-    public void splitFormula(String formula){
+    private void splitFormula(String formula) throws Exception {
         String tempRes = formula.trim();
-        for (Character ch: tempRes.toCharArray()
-             ) {
-            if (Character.isDigit(ch)){
+        String tempOp;
+        for (char ch : tempRes.toCharArray()
+                ) {
+            if (Character.isDigit(ch)) {
                 numStack.push(Character.getNumericValue(ch));
-            }else{
-                opStack.push(ch);
+            } else {
+                tempOp = String.valueOf(ch);
+                if (opStack.isEmpty() ||
+                        PriorityCheck.check(tempOp) > PriorityCheck.check(opStack.getFirst())) {
+                    opStack.push(tempOp);
+                } else {
+                    //To do
+
+                }
             }
         }
         System.out.println(numStack);
@@ -20,10 +29,30 @@ public class Test {
     }
 
 
+    private int calcWithOp(String Op, int pre, int post) {
+
+        switch (Op) {
+            case "+":
+                return pre + post;
+            case "-":
+                return pre - post;
+            case "*":
+                return pre * post;
+            case "/":
+                return pre / post;
+            default:
+                throw new RuntimeException("Op error");
+        }
+    }
+
 
     public static void main(String[] args) {
 //        System.out.println(Character.isDigit('2'));
         Test test = new Test();
-        test.splitFormula("ass");
+        try {
+            test.splitFormula(args[0]);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
